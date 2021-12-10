6f5e6a88422544182b64bc57465c3adca534479c
Oscar Urizar <oscar.m.Urizar.jr@gmail.com>
2021-12-10 14:21:37 -0500

    app is now working

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,35 +1,31 @@
+// ************Using function components*************
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { v4 as uuidv4 } from "uuid";
 import "./form.css";
 
 function Form() {
   const [name, setName] = useState("");
   const [uuid, setUuid] = useState("");
 
-  const { v4: uuidv4 } = require("uuid");
-
-  const handleFormSubmit = (e) => {
+  const handleSubmit = (e) => {
     // Preventing the default behavior of the form submit
     e.preventDefault();
 
     // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
+    const inputValue = e.target.name.value;
 
     // use input to save
-    if (inputType === "name") {
-      setName(inputValue);
-      setUuid(uuidv4());
-    }
+    setName(inputValue);
+    setUuid(uuidv4());
   };
 
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row instructions text-left">
-          <div className="col-sm-2 d-flex justify-content-between"></div>
-          <div className="col-sm-10 d-flex justify-content-between">
+          <div className=" col-sm-2 d-flex justify-content-between"></div>
+          <div className=" col-sm-10 d-flex justify-content-between">
             <label>Please enter your name below:</label>
           </div>
         </div>
@@ -43,38 +39,29 @@ function Form() {
             <div className="row">
               <input
                 name="name"
-                type="text"
-                onChange={handleFormSubmit}
+                type="name"
                 className="form-control"
-                id="name"
-                placeholder="Type your name here (with no spaces)"
+                placeholder="Type your name here"
               ></input>
             </div>
             <div className="row">
-              <button
-                name="button"
-                type="submit"
-                className="btn-success btn-lg"
-                onSubmit={handleFormSubmit}
-              >
-                Clear
+              <button type="submit" className="btn-success btn-lg">
+                Submit
               </button>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-sm-2 label-font">
-            <label className="label-font">
-              Result:
-            </label>
-          </div>
-          <div className="col-sm-10 result-output">
-            <label value={name} className="result-output">
-              {name}: {uuid}
-            </label>
-          </div>
-        </div>
       </form>
+      <div className="row">
+        <div className=" col-sm-2 label-font">
+          <label className="label-font">Result:</label>
+        </div>
+        <div className=" col-sm-10 result-output">
+          <label className="result-output">
+            {name}: {uuid}
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
